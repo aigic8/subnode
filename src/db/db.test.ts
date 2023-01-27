a0a7d7e9c12cad510599abe31b25b54b55153e1e
aigic8 <aigic.dev@protonmail.com>
2023-01-27 07:00:39 +0330

    refactored db.test.ts

--- src/db/db.test.ts
+++ src/db/db.test.ts
@@ -4,11 +4,6 @@ import { NewDB } from './db'
 const DB_URI = 'mongodb://127.0.0.1:2727/'
 const DB_NAME = 'subnode_test'
 
-// TODO IMPORTANT: create a initlize db with starting parameters and save us from this hell
-// Like initializeDbWith(db, {
-// 	projects: ["jkjljk"],
-// })
-
 test.serial('initializes db without errors', async t => {
 	const db = NewDB(DB_URI, DB_NAME)
 	await t.notThrowsAsync(db.initialize())
@@ -16,18 +11,14 @@ test.serial('initializes db without errors', async t => {
 })
 
 test.serial('createProject should create project without errors', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
+	const db = await initDB()
 
 	await db.createProject('memoryleaks')
 	t.pass()
 })
 
 test.serial('createProject should error if project DOES exist', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
+	const db = await initDB()
 
 	const projectName = 'memoryleaks'
 	await db.createProject(projectName)
@@ -35,14 +26,10 @@ test.serial('createProject should error if project DOES exist', async t => {
 })
 
 test.serial('getProject should work', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-	const dbProject = await db.getProject(projectName)
+	const db = await initDB({ projects: [projectName] })
 
+	const dbProject = await db.getProject(projectName)
 	if (!dbProject) {
 		t.fail('db project was null')
 		return
@@ -53,21 +40,15 @@ test.serial('getProject should work', async t => {
 })
 
 test.serial("getProject should return null when project doesn't exist", async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
+	const db = await initDB()
 
 	const dbProject = await db.getProject('memoryleaks')
 	t.is(dbProject, null)
 })
 
 test.serial('setProjectLastDnsProbed should work', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
+	const db = await initDB({ projects: [projectName] })
 
 	const date = new Date()
 	date.setDate(date.getDate() + 2)
@@ -85,9 +66,7 @@ test.serial('setProjectLastDnsProbed should work', async t => {
 test.serial(
 	'setProjectLastDnsProjebed should fail if project DOES NOT exist',
 	async t => {
-		const db = NewDB(DB_URI, DB_NAME)
-		await db.initialize()
-		await db.DEBUG_cleanDB()
+		const db = await initDB()
 
 		const projectName = 'memoryleaks'
 		const date = new Date()
@@ -96,12 +75,8 @@ test.serial(
 )
 
 test.serial('upsertNewRootDomains should not fail', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
+	const db = await initDB({ projects: [projectName] })
 
 	await t.notThrowsAsync(
 		db.upsertNewRootDomains(projectName, ['memoryleaks.ir', 'memoryleaks2.ir'])
@@ -109,15 +84,12 @@ test.serial('upsertNewRootDomains should not fail', async t => {
 })
 
 test.serial('getRootDomains should not fail', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomains = ['memoryleaks.ir', 'memoryleaks2.ir']
-	await db.upsertNewRootDomains(projectName, rootDomains)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+	})
 
 	const dbRootDomains = (await db.getRootDomains(projectName)).map(
 		item => item.rootDomain
@@ -127,27 +99,21 @@ test.serial('getRootDomains should not fail', async t => {
 })
 
 test.serial('getRootDomains should not fail with no rootDomains', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
+	const db = await initDB({ projects: [projectName] })
 
 	const dbRootDomains = await db.getRootDomains(projectName)
 	t.is(dbRootDomains.length, 0)
 })
 
 test.serial('upsertNewSubdomains should not fail', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
+	const rootDomains = [rootDomain]
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+	})
 
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
 	await t.notThrowsAsync(
@@ -159,21 +125,18 @@ test.serial('upsertNewSubdomains should not fail', async t => {
 })
 
 test.serial('getSubdomains should not fail', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const dbSubdomains = await db.getSubdomains(projectName)
 
@@ -189,21 +152,18 @@ test.serial('getSubdomains should not fail', async t => {
 })
 
 test.serial('getSubdomains after date should be applied', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const after = new Date()
 	after.setDate(after.getDate() + 2)
@@ -212,42 +172,36 @@ test.serial('getSubdomains after date should be applied', async t => {
 })
 
 test.serial('getSubdomainsWithIp should not fail', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const subdomainsWithIP = await db.getSubdomainsWithIp(projectName)
 	t.is(subdomainsWithIP.length, 0)
 })
 
 test.serial('getSubdomainsWithIp after date should be used', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const subWithIP = 'wp.memoryleaks.ir'
 	const errors = await db.ensureSubdomainsDNSState(projectName, {
@@ -262,21 +216,18 @@ test.serial('getSubdomainsWithIp after date should be used', async t => {
 })
 
 test.serial('ensureSubdomainDNSState should work', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const subWithIP = 'wp.memoryleaks.ir'
 	const errors = await db.ensureSubdomainsDNSState(projectName, {
@@ -293,21 +244,18 @@ test.serial('ensureSubdomainDNSState should work', async t => {
 test.serial(
 	'ensureSubdomainDNSState should not fail if subdomain already have ip',
 	async t => {
-		const db = NewDB(DB_URI, DB_NAME)
-		await db.initialize()
-		await db.DEBUG_cleanDB()
-
 		const projectName = 'memoryleaks'
-		await db.createProject(projectName)
-
 		const rootDomain = 'memoryleaks.ir'
-		await db.upsertNewRootDomains(projectName, [rootDomain])
-
+		const rootDomains = [rootDomain]
 		const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-		await db.upsertNewSubdomains(
-			projectName,
-			subdomains.map(subdomain => ({ rootDomain, subdomain }))
-		)
+		const db = await initDB({
+			projects: [projectName],
+			rootDomains: { project: projectName, rootDomains },
+			subdomains: {
+				project: projectName,
+				subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+			},
+		})
 
 		const subWithIP = 'wp.memoryleaks.ir'
 		const errors = await db.ensureSubdomainsDNSState(projectName, {
@@ -331,42 +279,36 @@ test.todo(
 )
 
 test.serial('getSubdomainsWithHTTP should not fail', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const subdomainsWithHTTP = await db.getSubdomainsWithHTTP(projectName)
 	t.is(subdomainsWithHTTP.length, 0)
 })
 
 test.serial('getSubdomainsWithHTTP after date should be used', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const subWithHTTP = 'wp.memoryleaks.ir'
 	const errors = await db.ensureSubdomainsHTTPState(projectName, {
@@ -381,21 +323,18 @@ test.serial('getSubdomainsWithHTTP after date should be used', async t => {
 })
 
 test.serial('ensureSubdomainsHTTPState should work', async t => {
-	const db = NewDB(DB_URI, DB_NAME)
-	await db.initialize()
-	await db.DEBUG_cleanDB()
-
 	const projectName = 'memoryleaks'
-	await db.createProject(projectName)
-
 	const rootDomain = 'memoryleaks.ir'
-	await db.upsertNewRootDomains(projectName, [rootDomain])
-
+	const rootDomains = [rootDomain]
 	const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-	await db.upsertNewSubdomains(
-		projectName,
-		subdomains.map(subdomain => ({ rootDomain, subdomain }))
-	)
+	const db = await initDB({
+		projects: [projectName],
+		rootDomains: { project: projectName, rootDomains },
+		subdomains: {
+			project: projectName,
+			subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+		},
+	})
 
 	const subsWithHTTP = 'wp.memoryleaks.ir'
 	const errors = await db.ensureSubdomainsHTTPState(projectName, {
@@ -412,21 +351,18 @@ test.serial('ensureSubdomainsHTTPState should work', async t => {
 test.serial(
 	'ensureSubdomainsHTTPState should not fail if subdomain already have http',
 	async t => {
-		const db = NewDB(DB_URI, DB_NAME)
-		await db.initialize()
-		await db.DEBUG_cleanDB()
-
 		const projectName = 'memoryleaks'
-		await db.createProject(projectName)
-
 		const rootDomain = 'memoryleaks.ir'
-		await db.upsertNewRootDomains(projectName, [rootDomain])
-
+		const rootDomains = [rootDomain]
 		const subdomains = ['wp.memoryleaks.ir', 'app1.memoryleaks.ir']
-		await db.upsertNewSubdomains(
-			projectName,
-			subdomains.map(subdomain => ({ rootDomain, subdomain }))
-		)
+		const db = await initDB({
+			projects: [projectName],
+			rootDomains: { project: projectName, rootDomains },
+			subdomains: {
+				project: projectName,
+				subdomains: subdomains.map(subdomain => ({ rootDomain, subdomain })),
+			},
+		})
 
 		const subsWithHTTP = 'wp.memoryleaks.ir'
 		const errors = await db.ensureSubdomainsHTTPState(projectName, {
@@ -448,6 +384,36 @@ test.serial(
 test.todo(
 	'ensureSubdomainsHTTPState should have error with a subdomain which does not exist'
 )
+
+interface DBInitialData {
+	projects?: string[]
+	rootDomains?: { project: string; rootDomains: string[] }
+	subdomains?: {
+		project: string
+		subdomains: { rootDomain: string; subdomain: string }[]
+	}
+}
+async function initDB(initialData?: DBInitialData) {
+	const db = NewDB(DB_URI, DB_NAME)
+	await db.initialize()
+	await db.DEBUG_cleanDB()
+
+	if (initialData?.projects) {
+		await Promise.all(initialData.projects.map(db.createProject))
+	}
+
+	if (initialData?.rootDomains) {
+		const { project, rootDomains } = initialData.rootDomains
+		await db.upsertNewRootDomains(project, rootDomains)
+	}
+
+	if (initialData?.subdomains) {
+		const { project, subdomains } = initialData.subdomains
+		await db.upsertNewSubdomains(project, subdomains)
+	}
+
+	return db
+}
 
 function arrsAreEqual(arr1: string[], arr2: string[]): void {
 	if (arr1.length !== arr2.length)
